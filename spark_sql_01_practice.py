7bd29f2e1271f107f98d00be0af13667319dbbe3
gyanendar <84815283+gyanendar@users.noreply.github.com>
2021-10-31 19:53:05 +0000

    Update spark_sql_01_practice.py
    some simplification

--- spark_sql_01_practice.py
+++ spark_sql_01_practice.py
@@ -59,32 +59,41 @@ def my_main(spark,
     # ---------------------------------------
     
     
-    r1DF = inputDF.select(f.col("*")).where((f.col("latitude") >= south) & (f.col("latitude")<=north) &(f.col("longitude") >= west) & (f.col("longitude")<=east))
-    r2DF = r1DF.select(f.col("*")).where((f.dayofweek(f.col("date")) >1) & (f.dayofweek(f.col("date")) <7))
-    r3DF = r2DF.withColumn("filter_hrs",f.substring("date",12,2))
-    r4DF = r3DF.withColumn("total",f.lit(1))
+    filteredDF = inputDF.select(f.col("*")).where((f.col("latitude") >= south) & \
+                                            (f.col("latitude")<=north) & \
+                                            (f.col("longitude") >= west) & \
+                                            (f.col("longitude")<=east) & \
+                                            (f.dayofweek(f.col("date")) >1) & \
+                                            (f.dayofweek(f.col("date")) <7))
+
+    
+    hrsCOlDF = filteredDF.withColumn("filter_hrs",f.substring("date",12,2))               
 
     
     # 2. Operation C1: Creation createDataFrame
     hrs_listDF = spark.createDataFrame(map(lambda x:pyspark.sql.types.Row(x),hours_list),["hrs"])
-    filteredDF = r4DF.join(hrs_listDF,
-                              r4DF["filter_hrs"] == hrs_listDF["hrs"],
+    filteredDF = hrsCOlDF.join(hrs_listDF,
+                              hrsCOlDF["filter_hrs"] == hrs_listDF["hrs"],
                               "inner"
                               )
-    resultDF = filteredDF.drop("filter_hrs")
-    q5DF = resultDF.select(f.col("hrs").alias("hour"),f.col("congestion"),f.col("total"))
-    q6DF = q5DF.groupBy(["hour"]).agg({"congestion":"sum","total":"count"})\
-                                .withColumnRenamed("count(total)", "numMeasurements")\
+    
+    q5DF = filteredDF.select(f.col("hrs").alias("hour"),f.col("congestion"),f.col("congestion").alias("cong"))
+    
+    q6DF = q5DF.groupBy(["hour"]).agg({"congestion":"sum","*":"count","cong":"avg"})\
+                                .withColumnRenamed("count(1)", "numMeasurements")\
                                 .withColumnRenamed("sum(congestion)", "congestionMeasurements")\
-                                .orderBy(q5DF["hour"].asc())  
-    q7DF = q6DF.withColumn("percentage",f.round(100*f.col("congestionMeasurements")/f.col("numMeasurements"),2))
-
-    #q3DF = q2DF.select(f.col("hrs").alias("hour"),f.round(f.col("congestionMeasurements")/f.col("numMeasurements"),2).alias("percentage"),f.col("numMeasurements"),f.col("congestionMeasurements") ) 
-    q8DF = q7DF.select(f.col("hour"),f.col("percentage"),f.col("numMeasurements"),f.col("congestionMeasurements"))                                    
+                                .withColumnRenamed("avg(cong)", "percentage")                                
+    
+    
+    solutionDF=   q6DF.select(f.col("hour"),\
+                             (f.round(f.col("percentage")*100,2)).alias("percentage"),\
+                              f.col("numMeasurements"),\
+                              f.col("congestionMeasurements")).orderBy(q6DF["percentage"].desc())                       
+                      
     # ---------------------------------------
     
     # Operation A1: 'collect'
-    resVAL = q8DF.collect()
+    resVAL = solutionDF.collect()
     for item in resVAL:
         print(item)
 
@@ -117,8 +126,8 @@ if __name__ == '__main__':
     my_local_path = "../../../../3_Code_Examples/L07-23_Spark_Environment/"
     my_databricks_path = "/"
 
-    my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/my_dataset_complete/"
-    #my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/A01_ex1_micro_dataset_1/"
+    #my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/my_dataset_complete/"
+    my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/A01_ex1_micro_dataset_1/"
     #my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/A01_ex1_micro_dataset_2/"
     #my_dataset_dir = "C:/gyani/Msc/BigData/A01_Datasets/A01_ex1_micro_dataset_3/"
 
