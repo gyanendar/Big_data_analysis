fdea0cd53b080740f89d5a3c02821700fb3c0e74
gyanendar <84815283+gyanendar@users.noreply.github.com>
2021-11-08 11:23:08 +0000

    Update A01_ex2_spark_sql_practice.py
    save

--- A01_ex2_spark_sql_practice.py
+++ A01_ex2_spark_sql_practice.py
@@ -19,6 +19,7 @@
 import pyspark
 import time
 import pyspark.sql.functions as f
+from pyspark.sql.window import Window
 
 # ------------------------------------------
 # FUNCTION my_main
@@ -55,25 +56,60 @@ def my_main(spark,
     # ---------------------------------------
     # TO BE COMPLETED
     # ---------------------------------------
-    r1DF = inputDF.select(f.col("*")).where((f.col("atStop") != 0) & (f.col("vehicleID") == vehicle_id) &(f.to_date(f.lit(day_picked),"yyyy-MM-dd") == f.to_date(inputDF["date"],"yyyy-MM-dd HH:mm:ss")) )
+    
+    # Apply first filtering on date, vechile id, and atStop
+    filteredDF = inputDF.select(f.col("*")).where((f.col("atStop") != 0) & \
+                                            (f.col("vehicleID") == vehicle_id) &\
+                                            (f.to_date(f.lit(day_picked),"yyyy-MM-dd") == f.to_date(inputDF["date"],"yyyy-MM-dd HH:mm:ss")) )
+    # Sort the rows based on date
+    timesortedDF= filteredDF.orderBy(f.to_timestamp(filteredDF["date"]))
 
-    rxDF= r1DF.orderBy(f.to_timestamp(r1DF["date"]))
+    
+    # Add new column having time of date and rename the existing column based on expected final output
+    # Select the necessary columns only
+    timeStampDF = timesortedDF.select(f.col("busLineID").alias("lineID"),f.col("closerStopID").alias("stationID"),f.col("delay"),f.col("date"))\
+               .withColumn("arrivalTime",f.date_format(timesortedDF["date"],"HH:mm:ss"))
 
-    #ryDF = rxDF.dropDuplicates(["busLineID","closerStopID"])
-
-    r2DF = rxDF.select(f.col("busLineID").alias("lineID"),f.col("closerStopID").alias("stationID"),f.col("delay"),f.col("date"))\
-               .withColumn("arrivalTime",f.date_format(r1DF["date"],"HH:mm:ss"))
-    r3DF = r2DF.select(f.col("lineID"),f.col("stationID"),f.col("arrivalTime"),f.col("delay"))\
+    #order the row based on time stamp columns arrivalTime
+    datatoWorkDF = timeStampDF.select(f.col("lineID"),f.col("stationID"),f.col("arrivalTime"),f.col("delay"))\
                .orderBy(f.col("arrivalTime"))
-    r4DF = r3DF.withColumn("onTime",f.when(f.abs(r3DF["delay"])>=delay_limit,0)\
+    
+    # Add delay flag column as per expected output
+    delayFlagAddedDF = datatoWorkDF.withColumn("onTime",f.when(f.abs(datatoWorkDF["delay"])>=delay_limit,0)\
                .otherwise(1))
-    r5DF = r4DF.select(f.col("lineID"),f.col("stationID"),f.col("arrivalTime"),f.col("onTime"))
-    r6DF = r5DF.dropDuplicates(["lineID","stationID"])    
-       
+    # discard delay column
+    finalWorkingDF = delayFlagAddedDF.select(f.col("lineID"),f.col("stationID"),f.col("arrivalTime"),f.col("onTime"))
+    
+    # Generate row number as coulmn
+    w = Window().orderBy(f.lit('A'))
+    dataWithRowNumberDF = finalWorkingDF.withColumn("row_num", f.row_number().over(w))
+
+    #clone dataWithRowNumberDF with renamed column name to differentiate bewteen columns coming from different DF
+    cloneDF = dataWithRowNumberDF.select(f.col("lineID").alias("lineID_1"),\
+                                      f.col("stationID").alias("stationID_1"),\
+                                      f.col("arrivalTime").alias("arrivalTime_1"),\
+                                      f.col("onTime").alias("onTime_1"),\
+                                      f.col("row_num").alias("row_num_1"))
+    
+    # Here need rows based on incrementing time 
+    # Doing cross join to get all combination                                      
+    multipliedDF = dataWithRowNumberDF.crossJoin(cloneDF)  
+
+    # Row number generated from 1..N
+    # line id& station id having no repeatition
+    # Need to keep the combination where 2nd part coming as next rownumber 
+    keepNextRowDF = multipliedDF.select(f.col("*")).where((multipliedDF["row_num_1"]-multipliedDF["row_num"]==1)|((multipliedDF["row_num_1"]==1)&(multipliedDF["row_num"]==1)))   
+     
+    # filter out the row here station id and line is matching 
+    removeMatchingLineStationIDDF = keepNextRowDF.select(f.col("*")).where(~((f.col("lineID") == f.col("lineID_1"))& (f.col("stationID") == f.col("stationID_1")))|((f.col("row_num_1")==1)&(f.col("row_num")==1)))
+
+    # Rename the columns as per expected output
+    solutionDF = removeMatchingLineStationIDDF.select(f.col("lineID_1").alias("lineID"),f.col("stationID_1").alias("stationID"),f.col("arrivalTime_1").alias("arrivalTime"),f.col("onTime_1").alias("onTime"))
+    
     # ---------------------------------------
     
     # Operation A1: 'collect'
-    resVAL = r6DF.collect()
+    resVAL = solutionDF.collect()
     for item in resVAL:
         print(item)
 
@@ -126,3 +162,4 @@ if __name__ == '__main__':
 
     total_time = time.time() - start_time
     print("Total time = " + str(total_time))
+
